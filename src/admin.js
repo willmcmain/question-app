0e2f252f713b9b88a9a5b3324e1ed38e9c08b0e7
Will McMain <willmcmain@gmail.com>
2017-03-12 18:23:12 -0400

    Edit questions

--- src/admin.js
+++ src/admin.js
@@ -2,7 +2,7 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Modal, Button} from 'react-bootstrap';
+import {FormControl, FormGroup, InputGroup, ControlLabel, Modal, Button, Glyphicon} from 'react-bootstrap';
 
 export default class QuestionAdmin extends React.Component {
     constructor() {
@@ -36,12 +36,23 @@ export default class QuestionAdmin extends React.Component {
         })
         .done(function(data) {
             that.load_questions();
+        })
+        .fail(function(jqXHR, textStatus) {
+            alert("DELETE Request failed: " + textStatus);
         });
     }
 
-    update_question(qid) {
-        alert("Update " + qid);
-        this.forceUpdate();
+    update_question(question) {
+        var that = this;
+        $.ajax({
+            url: '/questions/' + question.qid + '/',
+            method: 'PUT',
+            data: question,
+            traditional: true
+        })
+        .done(function(data) {
+            that.load_questions();
+        })
     }
 
     render() {
@@ -74,6 +85,7 @@ function QuestionEntry(props) {
     return (
         <tr>
           <td>
+            <EditBox question={props.question} onupdate={props.onupdate} />
             <DeleteBox question={props.question} ondelete={props.ondelete} />
           </td>
           <td>{props.question.question}</td>
@@ -107,24 +119,144 @@ class DeleteBox extends React.Component {
 
     render() {
         return (
-            <div>
-                <span className='glyphicon glyphicon-trash'
-                  onClick={() => this.open()}>
-                </span>
+          <div style={ {display: 'inline-block'} }>
+            <Button onClick={() => this.open()}>
+              <Glyphicon glyph='trash' />
+            </Button>
 
-                <Modal show={this.state.show} onHide={() => this.close()}>
-                  <Modal.Header>
-                    <Modal.Title>Really Delete This Question?</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>{this.props.question.question}</Modal.Body>
-                  <Modal.Footer>
-                    <Button bsStyle='danger' onClick={()=>this.delete_q()}>
-                      Delete
-                    </Button>
-                    <Button onClick={()=>this.close()}>Cancel</Button>
-                  </Modal.Footer>
-                </Modal>
-            </div>
+            <Modal show={this.state.show} onHide={() => this.close()}>
+              <Modal.Header>
+                <Modal.Title>Really Delete This Question?</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>{this.props.question.question}</Modal.Body>
+              <Modal.Footer>
+                <Button bsStyle='danger' onClick={()=>this.delete_q()}>
+                  Delete
+                </Button>
+                <Button onClick={()=>this.close()}>Cancel</Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        )
+    }
+}
+
+
+class EditBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+            // deep copy question object so we don't clobber the state
+            question: $.extend(true, {}, props.question)
+        }
+    }
+
+    open() {
+        this.setState({show: true});
+    }
+
+    close() {
+        // reload original question object; we want to lose our changes if
+        // we close the edit box without saving
+        this.setState({
+            show: false,
+            question: $.extend(true, {}, this.props.question)
+        });
+    }
+
+    update() {
+        this.props.onupdate(this.state.question);
+        this.close();
+    }
+
+    formHandler(e) {
+        var question = this.state.question;
+        question[e.target.name] = e.target.value;
+        this.setState({question: question});
+    }
+
+    distractorHandler(i, e) {
+        var question = this.state.question;
+        question.distractors[i] = e.target.value;
+        this.setState({question: question})
+    }
+
+    addDistractor() {
+        var question = this.state.question;
+        question.distractors = question.distractors.concat(['']);
+        this.setState({question: question})
+    }
+
+    removeDistractor(i) {
+        var question = this.state.question;
+        question.distractors = question.distractors.slice();
+        question.distractors.splice(i, 1);
+        this.setState({question: question})
+    }
+
+    render() {
+        return (
+          <div style={ {display: 'inline-block'} }>
+            <Button onClick={() => this.open()}>
+              <Glyphicon glyph='pencil' />
+            </Button>
+
+            <Modal show={this.state.show} onHide={() => this.close()}>
+              <Modal.Header>
+                <Modal.Title>Edit Question</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <form>
+                <FormGroup controlId='question'>
+                  <ControlLabel>Question</ControlLabel>
+                  <FormControl
+                    name='question'
+                    type='text'
+                    value={this.state.question.question}
+                    onChange={(e) => this.formHandler(e)}
+                  />
+                </FormGroup>
+                <FormGroup controlId='answer'>
+                  <ControlLabel>Answer</ControlLabel>
+                  <FormControl
+                    name='answer'
+                    type='text'
+                    value={this.state.question.answer}
+                    onChange={(e) => this.formHandler(e)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <ControlLabel>Wrong Answers</ControlLabel>
+                  {this.state.question.distractors.map((distractor, i) =>
+                      <InputGroup key={i}>
+                        <FormControl
+                          name={'distractor' + i}
+                          type='text'
+                          value={distractor}
+                          onChange={(e) => this.distractorHandler(i,e)}
+                        />
+                        <InputGroup.Button>
+                          <Button onClick={()=>this.removeDistractor(i)}>
+                          <Glyphicon glyph="minus" />
+                          </Button>
+                        </InputGroup.Button>
+                      </InputGroup>
+                  )}
+                </FormGroup>
+                <Button onClick={()=>this.addDistractor()}>
+                  <Glyphicon glyph='plus' />
+                </Button>
+                </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button bsStyle='primary' onClick={()=>this.update()}>
+                  Update
+                </Button>
+                <Button onClick={()=>this.close()}>Cancel</Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         )
     }
 }
